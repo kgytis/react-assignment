7002d37d7b37175fb0e62cb3d18fefe8a5cc609b
kgytis <kaziukon.gytis@gmail.com>
2022-07-08 13:49:11 +0300

    Page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// Pages imports
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Blogs from "./components/pages/Blogs";
+import Blog from "./components/pages/Blog";
+// Router imports
+import { Routes, Route } from "react-router-dom";
+import NewBlog from "./components/pages/NewBlog";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/newBlog" element={<NewBlog />} />
+        <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+    </>
   );
 }
 
