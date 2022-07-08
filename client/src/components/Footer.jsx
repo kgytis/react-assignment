4a2f4898a8a8f331c31ed652c0216e2e2066536f
kgytis <kaziukon.gytis@gmail.com>
2022-07-08 16:18:49 +0300

    Footer creation

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../assets/styles/Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Copyright kGytis. <br />
+        2022-07-08{" "}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
